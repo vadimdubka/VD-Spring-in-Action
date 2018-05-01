2d49bcafb710c52ea92a11449cd2204f425ed06f
vadimdubka <vadimdubka@gmail.com>
2018-05-01 17:43:24 +0300

    7.5.2	Working with flash attributes

--- src/main/java/com/vadimdubka/spittr/web/SpitterController.java
+++ src/main/java/com/vadimdubka/spittr/web/SpitterController.java
@@ -14,12 +14,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping("/spitter")
@@ -38,7 +41,7 @@ public class SpitterController {
     }
     
     /*5.4	Processing forms*/
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    @RequestMapping(value = "/register", method = GET)
     public String showRegistrationForm(Model model) {
         logger.debug("showRegistrationForm()");
         model.addAttribute(new Spitter("name",
@@ -50,7 +53,7 @@ public class SpitterController {
     }
     
     /*Example for thymeleaf*/
-    @RequestMapping(value = "/thymeleaf/register", method = RequestMethod.GET)
+    @RequestMapping(value = "/thymeleaf/register", method = GET)
     public String thymeleafShowRegistrationForm(Model model) {
         logger.debug("thymeleafShowRegistrationForm()");
         model.addAttribute(new Spitter("th-name",
@@ -61,12 +64,20 @@ public class SpitterController {
         return "thymeleaf/registerForm";
     }
     
-    /*5.4.1	Writing a form-handling controller*/
-    /*5.4.2	Validating forms*/
-    /*7.2.2	Handling multipart requests*/
+    /**
+     * 5.4.1	Writing a form-handling controller
+     * 5.4.2	Validating forms
+     * 7.2.2	Handling multipart requests
+     * 7.5.2	Working with flash attributes (instead of {@link Model} we use {@link RedirectAttributes}
+     * to save request attributes to session before redirect and retrieve them from the session automatically.
+     * Spring agrees that putting data into the session is a great way to pass information that survives a redirect.
+     * But Spring doesn’t think you should be responsible for managing that data.
+     * Flash attributes are stored in the session and carrying data until the next request,
+     * after request they are retrieved into the model, surviving a redirect; then they go away.
+     */
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public String processRegistration(@RequestParam("profilePicture") MultipartFile file,
-                                      @Valid Spitter spitter, Errors errors, Model model) {
+                                      @Valid Spitter spitter, Errors errors, RedirectAttributes model) {
         logger.debug("processRegistration()");
         if (errors.hasErrors()) {
             logger.error(errors.getAllErrors().toString());
@@ -87,7 +98,6 @@ public class SpitterController {
         /*First variant of redirect.
          * String concatenation is dangerous business when constructing things like URLs and SQL queries.*/
         // return "redirect:/spitter/" + spitter.getUsername();
-        
         /*Second variant of redirect - using URL templates
         7.5.1	Redirecting with URL templates.
         All you need to do is set the value in the model.
@@ -103,14 +113,19 @@ public class SpitterController {
         it’s tacked on to the redirect automatically as a query parameter.*/
         model.addAttribute("id", spitter.getId()); /*If the username attribute is habuma and the spitterId attribute is 42,
         then the resulting redirect path will be /spitter/habuma?spitterId=42.*/
+        model.addFlashAttribute("spitter", spitter);
         return "redirect:/spitter/{username}";
     }
     
-    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    /** 7.5.2	Working with flash attributes */
+    @RequestMapping(value = "/{username}", method = GET)
     public String showSpitterProfile(@PathVariable String username, Model model) {
         logger.debug("showSpitterProfile()");
-        Spitter spitter = spitterRepository.findByUsername(username);
-        model.addAttribute(spitter);
+        if (!model.containsAttribute("spitter")) {
+            logger.debug("looking for spitter in the repository");
+            Spitter spitter = spitterRepository.findByUsername(username);
+            model.addAttribute(spitter);
+        }
         return "spittles/profile";
     }
     
