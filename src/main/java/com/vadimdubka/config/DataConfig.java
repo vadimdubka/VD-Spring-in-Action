b6cc70db93d0faaaf5384194ee835b1768377bd6
vadimdubka <vadimdubka@gmail.com>
2018-06-02 16:07:44 +0300

    Feature 2 from branch 2 is added.

--- src/main/java/com/vadimdubka/config/DataConfig.java
+++ src/main/java/com/vadimdubka/config/DataConfig.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DataConfig {
     //TODO сделать один конфигурационный файл для DataSource и сконфигурировать там 2 DataSource для 2 частей приложения
+    //TODO added from feature2
     @Bean
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
